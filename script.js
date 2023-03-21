1b8331b2622732ebd019b3a5dfebf14291e48db9
MelissaB <121251098+melissa-studi@users.noreply.github.com>
2023-03-21 12:30:11 +0100

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-let alphabet = "abcdefghijklmnopqrstuvwxyz";
-console.log([...alphabet]);
- const isPangram = (sentence) => {
-    return [...alphabet].every(letter => letter.includes(sentence))
- }
